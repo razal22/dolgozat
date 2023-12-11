8873af00838e7058700f323686ada8b38c244ffa
razal22 <76556418+razal22@users.noreply.github.com>
2023-12-11 11:23:14 +0100

    Add files via upload

---
+++ Data/BookAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BookApp.Models;
+
+namespace BookApp.Data
+{
+    public class BookAppContext : DbContext
+    {
+        public BookAppContext (DbContextOptions<BookAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookApp.Models.Book> Book { get; set; } = default!;
+    }
+}
